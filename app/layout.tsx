df74e8cb72f7c635bd853282e7516abea70e9b87
Admin <vuchanhung2001@gmail.com>
2023-11-15 17:10:28 +0700

    create initial steps for project set up dark mode and authenticate through clark service

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
+const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +17,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem={false}
+              storageKey='discord-theme'
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          
+          </body>
+ 
+      </html>
+    </ClerkProvider>
   )
 }
